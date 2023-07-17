324a024193808f0cba77136134b2f43bc0dcd060
iceror <ilsecervor@gmail.com>
2023-07-17 09:45:50 -0600

    move test folder inside md-links-cli

---
+++ md-links-cli/test/md-links.spec.js
@@ -0,0 +1,14 @@
+const mdLinks = require('../index');
+
+
+describe('mdLinks', () => {
+
+  it('should...', () => {
+    console.log('FIX ME!');
+  });
+
+  it('should return a promise', () => {
+    expect(typeof mdLinks).toBe('function')
+  });
+
+});
