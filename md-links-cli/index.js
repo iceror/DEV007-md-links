38bad86c2a049aaeb241750c8a71c1bfde3be545
iceror <ilsecervor@gmail.com>
2023-07-17 09:46:07 -0600

    create mdLinks function

--- md-links-cli/index.js
+++ md-links-cli/index.js
@@ -4,6 +4,13 @@ const chalk = require("chalk")
 console.log(chalk.magenta('init chalk'));
 console.log(chalk.red('hello'));
 // CommonJS Modules require/module.exports 
+const mdLinks = (path, options) => {
+  // mdLinks debe retornar una promesa
+  const mdLinksPromise = new Promise((resolve, reject) => {
+
+  })
+  return mdLinksPromise
+}
 module.exports = () => {
   // ...
 };
