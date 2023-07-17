ee8d49b03831a952900c0741606c3c47be46fd5f
iceror <ilsecervor@gmail.com>
2023-07-17 16:59:40 -0600

    verify if path is absolute, get file extension

--- md-links-cli/index.js
+++ md-links-cli/index.js
@@ -1,20 +1,43 @@
 #!/usr/bin/env node
 // CommonJS Modules require/module.exports 
-
 const fs = require('fs')
+const path = require('path')
 const chalk = require("chalk")
-console.log(chalk.magenta('init chalk'));
-console.log(chalk.red('hello'));
 
-const mdLinks = (path, options) => {
+//console.log(process.argv);
+const mdLinks = (givenPath, options) => {
   // mdLinks debe retornar una promesa
   return new Promise((resolve, reject) => {
-    // mdLinks should check if the path exists. if it doesn't exists, reject Promise
-    if (fs.existsSync(path)) {
-      console.log('The path exists!');
+    // Verify if path is absolute, if relative, make it absolute
+    let absolutePath;
+    if (path.isAbsolute(givenPath)) {
+      absolutePath = givenPath;
+      console.log(true, absolutePath)
     } else {
-      reject('ERROR path is not valid');
+      absolutePath = path.resolve(givenPath);
+      console.log(false, absolutePath)
     }
+
+    let mdFilesArray = [];
+    // get file extension
+    if (path.extname(absolutePath) === '.md') {
+      mdFilesArray.push(absolutePath);
+      console.log(chalk.greenBright('extension is .MD'));
+    } else { 
+      console.log(chalk.redBright('File extension is not .MD'));
+    }
+
+    console.log(mdFilesArray);
+
+    // mdLinks should check if the path exists. if it doesn't exists, reject Promise
+    /* if (fs.existsSync(absolutePath)) {
+      console.log(chalk.greenBright('Path exists!!!', absolutePath));
+    } else {
+      console.log(chalk.red('ERROR path is not valid', absolutePath));
+    } */
+
+    // resolve devolver un array con el archivo
+
   });
 }
 
