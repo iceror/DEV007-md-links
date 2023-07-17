620f31134289c541e941e0fa69f8044760eddf21
iceror <ilsecervor@gmail.com>
2023-07-17 10:39:45 -0600

    create first tests

--- md-links-cli/test/md-links.spec.js
+++ md-links-cli/test/md-links.spec.js
@@ -1,14 +1,20 @@
-const mdLinks = require('../index');
-
+const mdLinks = require('../index.js');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
-  it('should return a promise', () => {
+  it('should be a function', () => {
     expect(typeof mdLinks).toBe('function')
   });
+
+  // it('should return a promise', () => {
+  //   expect(mdLinks()).toBe(typeof Promise)
+  // });
+
+  it('should reject the promise when path is not valid', () => {
+    console.log(mdLinks);
+    return mdLinks('/user/folder/path.md').catch((error) =>{
+      expect(error).toBe('ERROR path is not valid')
+    })
+  })
 
 });
