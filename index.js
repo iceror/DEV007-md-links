092ac112811b592cb143cb7f2d81ce517c74c62c
iceror <ilsecervor@gmail.com>
2023-07-21 10:20:21 -0600

    move code tu functions

--- index.js
+++ index.js
@@ -9,52 +9,110 @@ const mdLinks = (givenPath, options) => {
   // mdLinks debe retornar una promesa
   return new Promise((resolve, reject) => {
     // Verify if path is absolute. if relative, make it absolute
-    let absolutePath;
-    if (path.isAbsolute(givenPath)) {
-      absolutePath = givenPath;
-      console.log(true, absolutePath)
-    } else {
-      absolutePath = path.resolve(givenPath);
-      console.log(false, absolutePath)
-    }
+    let absolutePath = pathIsAbsolute(givenPath);
 
     // mdLinks should check if the path exists. if it doesn't exists, reject Promise
-    let existingPath
-    if (fs.existsSync(absolutePath)) {
-      existingPath = absolutePath
-      console.log(chalk.greenBright('Path exists!', absolutePath));
-    } else {
-      reject(chalk.red('ERROR path does not exist!', absolutePath));
-    }
+    let existingPath = pathExists(absolutePath);
 
     // check if path is directory 
-    let stats = fs.statSync(existingPath)
-    if (stats.isDirectory()) {
-      let directoryPath = existingPath
-      console.log(chalk.bgGreenBright('Path is directory', directoryPath));
-      // read files inside directory
-      const filesInDirectory = fs.readdirSync(directoryPath)
-      console.log(filesInDirectory);
-      // if it finds more directories, read files 
-      
-    } else {
-      existingPath
-      console.log(chalk.bgRedBright('Path is not directory', existingPath));
+    let directoryPath = pathIsDirectory(existingPath);
+    // check if path is file
+    let filePath = pathIsFile(existingPath);
+
+    let mdFilesArray;
+    if (filePath) {
+      mdFilesArray = getFileExtension(filePath);
+      readMdFiles(mdFilesArray)
+    } else if (directoryPath) {
+      getFilesInDirectory(directoryPath);
     }
 
-    let mdFilesArray = [];
-    // get file extension
-    if (path.extname(existingPath) === '.md') {
-      mdFilesArray.push(absolutePath);
-      console.log(chalk.greenBright('extension is .MD'));
-    } else {
-      console.log(chalk.redBright('File extension is not .MD'));
-    }
 
-    console.log(mdFilesArray);
+
+    console.log('INSIDE CLOG', directoryPath, filePath);
+
     // resolve devolver un array con el archivo
 
   });
 }
+
+function pathIsAbsolute(givenPath) {
+  let absolutePath;
+  if (path.isAbsolute(givenPath)) {
+    absolutePath = givenPath;
+    console.log('Absolute path', chalk.underline(absolutePath))
+  } else {
+    absolutePath = path.resolve(givenPath);
+    console.log('Relative path', chalk.underline(absolutePath))
+  }
+  return absolutePath
+}
+
+function pathExists(absolutePath) {
+  let existingPath;
+  if (fs.existsSync(absolutePath)) {
+    existingPath = absolutePath;
+    console.log(chalk.bgGreen('Path exists!', chalk.underline(absolutePath)));
+  } else {
+    console.log((chalk.bgRed('ERROR path does not exist!', chalk.strikethrough(absolutePath))));
+  }
+  return existingPath;
+}
+
+function pathIsDirectory(existingPath) {
+  let stats = fs.statSync(existingPath);
+  if (stats.isDirectory()) {
+    let directoryPath = existingPath;
+    console.log(chalk.green('Path is directory:', directoryPath));
+    return directoryPath
+  }
+}
+
+function getFilesInDirectory(directoryPath) {
+  // read files inside directory
+  const filesInDirectory = fs.readdirSync(directoryPath)
+  console.log('Found this files in directory: ', filesInDirectory);
+
+  // if it finds more directories, read files 
+  filesInDirectory.map((file => {
+    let fileExtension = getFileExtension(file);
+    if(!fileExtension){
+      // let notMdFile = pathIsFile(file)
+      console.log('not md');
+    }
+    // getFilesInDirectory(file)
+  })) 
+  // iterate inside filesInDirectory and get .md files 
+}
+
+function pathIsFile(existingPath) {
+  let stats = fs.statSync(existingPath);
+  if (stats.isFile()) {
+    let filePath = existingPath;
+    console.log(chalk.redBright('Path is not directory:', filePath));
+    //función para obtener extensión
+    // getFileExtension(filePath);
+    return filePath;
+  }
+}
+
+function getFileExtension(filePath) {
+  let mdFilesArray = [];
+  if (path.extname(filePath) === '.md') {
+    mdFilesArray.push(filePath);
+    console.log(chalk.greenBright('File extension is .MD'));
+    console.log(mdFilesArray);
+    return mdFilesArray
+  } else {
+    console.log(chalk.redBright('File extension is not .MD', filePath));
+  }
+}
+
+function readMdFiles(mdFilesArray) {
+  mdFilesArray.forEach((mdFile) => {
+    console.log(fs.readFileSync(mdFile, { encoding: 'utf8', flag: 'r' }))
+  })
+}
+
 
 module.exports = mdLinks 
