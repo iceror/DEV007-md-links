9cc350f59edfd05adf7394bbb50be0447ba8784d
iceror <ilsecervor@gmail.com>
2023-07-25 11:59:02 -0600

    change node line to cli.js (executable file)

--- cli.js
+++ cli.js
@@ -1,12 +1,10 @@
+#!/usr/bin/env node
 const mdLinks  = require('./index.js')
 
-// mdLinks('md-links-cli/index.js').then(() => {
-
-// }).catch((error) => {
-//   console.log(error);
-// })
-
-mdLinks('./mock-files').then(() => {
+console.log(process.argv);
+// conditional to check if options are passed 
+//make cli executable with md-links command not with node cli.js
+mdLinks(process.argv[2]).then(() => {
 
 }).catch((error) => {
   console.log(error);
