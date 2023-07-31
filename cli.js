fbdfe88dcd23cc4bdfa64391da9540b37763fa71
iceror <ilsecervor@gmail.com>
2023-07-31 10:00:52 -0600

    add options object

--- cli.js
+++ cli.js
@@ -4,8 +4,14 @@ const mdLinks  = require('./index.js')
 
 console.log(process.argv);
 // conditional to check if options are passed 
+const options = {};
+if(process.argv[3] === '--validate' || process.argv[4] === '--validate' ){
+  options.validate = true;
+} else {
+  options.validate = false;
+}
 //make cli executable with md-links command not with node cli.js
-mdLinks(process.argv[2]).then(() => {
+mdLinks(process.argv[2], options).then(() => {
 
 }).catch((error) => {
   console.log(error);
