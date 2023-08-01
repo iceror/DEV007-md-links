bd7408abca3874353da0df96d47599ec322183c7
iceror <ilsecervor@gmail.com>
2023-08-01 10:46:23 -0600

    create stats && brokenLinks fn

--- stats.js
+++ stats.js
@@ -1,5 +1,40 @@
-function stats(result){
-  console.log('Inside stats');
+function stats(result) {
+  // get Total and Unique links
+  let links = []
+  result.forEach(link => {
+    links.push(link.href)
+  });
+
+  const uniqueLinks = [];
+  const repeatedLinks = [];
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    const nextLink = links[i + 1];
+    if (link === nextLink) {
+      repeatedLinks.push(link);
+    } else {
+      uniqueLinks.push(link);
+    }
+  }
+
+  console.log('Total: ', result.length);
+  console.log('Unique: ', uniqueLinks.length);
 }
 
-module.exports = stats;
+function brokenLinks(result) {
+  let links = []
+  result.forEach(link => {
+    links.push(link.ok )
+  });
+
+  const broken = [];
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    if (link === 'fail') {
+      broken.push(link)
+    }
+  }
+  console.log('Broken: ',broken.length);
+}
+
+module.exports = { stats, brokenLinks };
